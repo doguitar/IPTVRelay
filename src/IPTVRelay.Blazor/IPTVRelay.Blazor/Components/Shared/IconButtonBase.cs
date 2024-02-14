048fffd898450d6c3b000c235103a63bd4fca278
doguitar <seanseymour@gmail.com>
2024-02-14 14:05:33 -0800

    1. Replaced existing button components with new custom button components in AddGuideModal.razor, AddMappingModal.razor, AddPlaylistModal.razor, Channels.razor, Guides.razor, Mapping.razor, Playlists.razor, and SettingsPage.razor files. 2. Modified the Filter component in AddMappingModal.razor and Filter.razor files to accept an additional type parameter, TItem, and a new parameter, Items. 3. Split the Generate method into two separate methods: Save and Generate in the Channels.razor file. 4. Added new CSS rules for the button.btn.btn-icon class in the app.css file. 5. Modified the project type of the IPTVRelay.Library.Components project in the IPTVRelay.sln file. 6. Created new custom button components in AddButton.razor, ChildButton.razor, CloseButton.razor, DeleteButton.razor, DownloadButton.razor, EditButton.razor, IconButton.razor, PlayButton.razor, RefreshButton.razor, and SaveButton.razor files. 7. Created a new base class for the custom button components in the IconButtonBase.cs file. 8. Modified the dropdown menu in the Filter component to skip the first item in the list of filter types in the Filter.razor file.

---
+++ src/IPTVRelay.Blazor/IPTVRelay.Blazor/Components/Shared/IconButtonBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace IPTVRelay.Blazor.Components.Shared
+{
+    public class IconButtonBase : ComponentBase
+    {
+        [Parameter] public bool Disabled { get; set; } = false;
+        [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+    }
+}
