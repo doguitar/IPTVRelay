4377df1ca5bfdc243800dd13ad470a0b2cab1e31
doguitar <doguitar@gmail.com>
2024-02-18 18:30:34 -0800

    consolidate filter functionality

--- src/IPTVRelay.Library.Components/Loader.cs
+++ src/IPTVRelay.Library.Components/Loader.cs
@@ -8,27 +8,38 @@ using System.Threading.Tasks;
 
 namespace IPTVRelay.Library.Components
 {
-    public class Loader : ComponentBase
-    {
-        [Parameter] public bool IsLoading { get; set; }
-        [Parameter] public string LoadingMessage { get; set; } = "Loading...";
-        [Parameter] public RenderFragment ChildContent { get; set; } = new RenderFragment(builder => { });
+	public class Loader : ComponentBase
+	{
+		[Parameter] public bool IsLoading { get; set; }
+		[Parameter] public bool HideContents { get; set; }
+		[Parameter] public string LoadingMessage { get; set; } = "Loading...";
+		[Parameter] public RenderFragment ChildContent { get; set; } = new RenderFragment(builder => { });
 
-        protected override void BuildRenderTree(RenderTreeBuilder builder)
-        {
-            if (IsLoading)
-            {
-                builder.OpenElement(0, "div");
-                builder.AddAttribute(1, "class", "loading-spinner");
-                builder.OpenElement(2, "div");
-                builder.AddAttribute(1, "class", "loading-spinner-message");
-                builder.AddContent(3, LoadingMessage);
-                builder.CloseElement();
-                builder.CloseElement();
-            }
-                base.BuildRenderTree(builder);
-                builder.AddContent(3, ChildContent);
-        }
+		protected override void BuildRenderTree(RenderTreeBuilder builder)
+		{
+			if (IsLoading)
+			{
+				builder.OpenElement(0, "div");
+				builder.AddAttribute(1, "class", "loading-spinner");
+				builder.OpenElement(2, "div");
+				builder.AddAttribute(1, "class", "loading-spinner-message");
+				builder.AddContent(3, LoadingMessage);
+				builder.CloseElement();
+				builder.CloseElement();
+			}
+			if (HideContents)
+			{
+				builder.OpenElement(0, "div");
+				builder.AddAttribute(1, "style", "display: none;");
+			}
+			base.BuildRenderTree(builder);
+			builder.AddContent(3, ChildContent);
 
-    }
+			if (HideContents)
+			{
+				builder.CloseElement();
+			}
+		}
+
+	}
 }
